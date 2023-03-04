4ea7b593e30209fa168d8d85e4a58575eaad30f8
Robert Vari <mail.robertvari@gmail.com>
2023-03-04 15:30:52 +0100

    update

--- say_hello.py
+++ say_hello.py
@@ -1,6 +1,7 @@
 name = input("What is your name?")
 email = input("Your email?")
-
+age = input("How old are you?")
 
 print(f"Hello {name}")
+print(f"Your age is {age}")
 print(f"You email is: {email}")
