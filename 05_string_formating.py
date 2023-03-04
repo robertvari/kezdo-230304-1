8cf389dc57f3e2cbb0d2adf9db9512195b0132cc
Robert Vari <mail.robertvari@gmail.com>
2023-03-04 15:19:20 +0100

    update

---
+++ 05_string_formating.py
@@ -0,0 +1,7 @@
+name = "Kiss Kriszta"
+address = "Siófok"
+email = "kriszta@gmail.com"
+
+print(f"Szia. A nevem {name}. Én {address}en élek. A mailcímen {email}")
+
+print(f"{name}, {address}, {email}")
