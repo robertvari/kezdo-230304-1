1bd5f87110bf9c83d2a8da0d93a0b9ad4bb2d7be
Robert Vari <mail.robertvari@gmail.com>
2023-03-04 16:40:33 +0100

    update

---
+++ 08_slicing.py
@@ -0,0 +1,10 @@
+my_friends = ["Robert", "Csilla", "Tamás", "Andi", "Balázs", "Karcsi"]
+my_name = "Robert Vari"
+folder_path = "/folder1/folder2/summer.jpg"
+
+print(my_friends[:3])
+print(my_friends[-3:])
+print(my_friends[2:4])
+print(my_friends[::-1])
+
+print(folder_path[1:-4])
