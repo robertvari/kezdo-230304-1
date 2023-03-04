ac424941cf0d0acf9b8d48bb799be4f67d9a59a2
Robert Vari <mail.robertvari@gmail.com>
2023-03-04 16:07:59 +0100

    update

---
+++ 07_list_methods.py
@@ -0,0 +1,21 @@
+my_friends = ["Robert", "Csilla", "Tamás"]
+
+print(my_friends)
+
+# add an item to the end of the list
+my_friends.append("András")
+print(my_friends)
+
+# remove an item from the list
+my_friends.remove("Robert")
+print(my_friends)
+
+# update item in a list
+name_index = my_friends.index("András")
+my_friends[name_index] = "Kiss Tamás"
+print(my_friends)
+
+# remove with index
+name_index = my_friends.index("Kiss Tamás")
+del my_friends[name_index]
+print(my_friends)
