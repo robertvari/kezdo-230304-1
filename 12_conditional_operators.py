2eded8e6626909a303bfd09193230a9e6fcc6823
Robert Vari <mail.robertvari@gmail.com>
2023-03-04 17:34:31 +0100

    Update

---
+++ 12_conditional_operators.py
@@ -0,0 +1,8 @@
+number = 11
+
+print(number == 10)     # False
+print(number != 10)     # True
+print(number < 10)      # False
+print(number > 10)      # True
+print(number >= 10)     # True
+print(number <= 10)     # False
