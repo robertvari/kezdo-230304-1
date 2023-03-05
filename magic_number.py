16063459a3f6556625d0f03f2b362c47f8c5222b
Robert Vari <mail.robertvari@gmail.com>
2023-03-05 15:18:09 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,27 +1,44 @@
 import os, random
 os.system("cls")
 
-tries = 3
+print("="*50, "MAGIC NUMBER GAME", "="*50)
+
+# set a min and max number for the dificulty
 min = 1
 max = 10
-magic_number = random.randint(min, max)
-
-print("="*50, "MAGIC NUMBER GAME", "="*50)
 print(f"I have a number between {min} and {max}. Can you guess it?")
-print(f"You have {tries} tries.")
 
-player_guess = input("Your guess?")
+# Start Game Loop
+while True:
+    # get a random number
+    magic_number = random.randint(min, max)
 
-while player_guess != str(magic_number):
-    os.system("cls")
-    tries -= 1
-    if tries == 0:
-        print(f"My number was {magic_number}.")
-        print("Game Over :(")
-        exit()
+    # set maximum tries to 3
+    tries = 3
+    print(f"You have {tries} tries.")
 
-    print(f"Wrong guess. You have {tries} tries left. Try again!")
+    # ask player guess
     player_guess = input("Your guess?")
 
-os.system("cls")
-print(f"You win! {magic_number} was my number! :)))")
+    # check player guess
+    while player_guess != str(magic_number):
+        os.system("cls")
+
+        # remove one tries
+        tries -= 1
+        if tries == 0:
+            print(f"My number was {magic_number}.")
+
+            player_choice = input("Do you want to play again? (y/n)")
+            if player_choice == "n":  
+                exit()
+
+        print(f"Wrong guess. You have {tries} tries left. Try again!")
+        player_guess = input("Your guess?")
+
+    os.system("cls")
+    print(f"You win! {magic_number} was my number! :)))")
+    
+    player_choice = input("Do you want to play again? (y/n)")
+    if player_choice == "n":  
+        exit()
